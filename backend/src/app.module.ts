830c4c5307300496e22982dd0c9746cda1ea4878
Le Duc Anh Tuan <tuanleqn04@gmail.com>
2024-10-13 13:10:30 +0000

    Revert "Merge branch 'be-login' into 'main'"
    This reverts merge request !4

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,10 +2,9 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TestModule } from './test/test.module';
-import { LoginModule } from './login/login.module';
 
 @Module({
-  imports: [TestModule, LoginModule],
+  imports: [TestModule],
   controllers: [AppController],
   providers: [AppService],
 })
