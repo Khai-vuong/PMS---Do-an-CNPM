1bd733d2b659ec670c22f978ea24ba864d09a89d
duongtrongkhoix3 <duongtrongkhoi.03@gmail.com>
2024-10-16 11:33:08 +0700

    request

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,17 +1,5 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "~/pages/Home";
-import Signup from "~/pages/Signup";
 function App() {
-  return (
-    <Router>
-      <div className="App">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/Signup" element={<Signup />} />
-        </Routes>
-      </div>
-    </Router>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
