6d305c9488a2dea62371abc6b1b1fe6ad89bc86c
Sierraaaaaaaaaaa <son.doanhoang@hcmut.edu.vn>
2024-10-19 15:50:36 +0700

    some change

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TestModule } from './test/test.module';
 import { LoginModule } from './login/login.module';
+import { LobbyModule } from './lobby/lobby.module';
 
 @Module({
-  imports: [TestModule, LoginModule],
+  imports: [TestModule, LoginModule, LobbyModule],
   controllers: [AppController],
   providers: [AppService],
 })
