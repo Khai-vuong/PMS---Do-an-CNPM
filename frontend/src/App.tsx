703fbf3cd475e8685d44e3e081ce6f297db513cb
Khai-vuong <khai.vuongquang@hcmut.edu.vn>
2024-10-19 12:02:47 +0700

    ??
    wtf?

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,21 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+
+import "./index.css";
+import Signup from "./pages/Signup/Signup.tsx";
+import Homepage from "./pages/Homepage/Homepage.tsx";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <>
+      <Router>
+        <Routes>
+          <Route path='/' element={<Homepage />}></Route>
+          <Route path='/auth/signup' element={<Signup />}></Route>
+
+        </Routes>
+      </Router>
+    </>
+  )
 }
 
 export default App;
--- frontend/src/pages/Homepage/Homepage.tsx
+++ frontend/src/pages/Homepage/Homepage.tsx
@@ -1,82 +1,75 @@
 //Homepage version made by V.Q.Khai
-import React from 'react';
 import { useNavigate } from 'react-router-dom';
-
-const navigate = useNavigate();
-
-const routeToLogin = () => {
-    navigate('/login', { replace: true });
-};
-
-const routeToSignup = () => {
-    navigate('/signup', { replace: true });
-};
-
-
-// const Homepage = () => {
-//     return (
-//         <>
-//             <div className="header">
-//                 <img src="" alt="ICON" />
-//                 <div className="buttons">
-//                     <button className="login-bt" onClick={routeToLogin}>Login</button>
-//                     <button className="signup-bt" onClick={routeToSignup}>Signup</button>
-//                 </div>
-//             </div>
-
-//             <div className="body">
-//                 <table>
-//                     <thead>
-//                         <tr>
-//                             <th>MSSV</th>
-//                             <th>Họ và tên</th>
-//                             <th>Công việc</th>
-//                         </tr>
-//                     </thead>
-//                     <tbody>
-//                         <tr>
-//                             <td>2211562</td>
-//                             <td>Vương Quang Khải</td>
-//                             <td>Team leader / PM</td>
-//                         </tr>
-//                         <tr>
-//                             <td>2213316</td>
-//                             <td>Lê Văn Thoại</td>
-//                             <td>Frontend dev</td>
-//                         </tr>
-//                         <tr>
-//                             <td>2113786</td>
-//                             <td>Dương Trọng Khôi</td>
-//                             <td>Frontend dev</td>
-//                         </tr>
-//                         <tr>
-//                             <td>2212935</td>
-//                             <td>Đoàn Ngọc Hoàng Sơn</td>
-//                             <td>Backend dev</td>
-//                         </tr>
-//                         <tr>
-//                             <td>2213772</td>
-//                             <td>Lê Đức Anh Tuấn</td>
-//                             <td>Backend dev</td>
-//                         </tr>
-//                     </tbody>
-//                 </table>
-//             </div>
-
-//             <div className="quote">
-//                 Coded with love and depression
-//             </div>
-//         </>
-
-
-
-//     );
-// };
+import './Homepage.css';
 
 const Homepage = () => {
+    const navigate = useNavigate();
+
+    const routeToLogin = () => {
+        navigate('auth/login', { replace: true });
+    };
+
+    const routeToSignup = () => {
+        navigate('auth/signup', { replace: true });
+    };
+
     return (
-        <h1>Hello</h1>
-    )
-}
+        <>
+            <div className="container">
+                <div className="header">
+                    <img src="" alt="ICON" />
+                    <div className="buttons">
+                        <button className="login-bt" onClick={routeToLogin}>Login</button>
+                        <button className="signup-bt" onClick={routeToSignup}>Signup</button>
+                    </div>
+                </div>
+
+                <div className="body">
+                    <h1 className="title">Project Management System</h1>
+                    <table>
+                        <thead>
+                            <tr>
+                                <th>MSSV</th>
+                                <th>Họ và tên</th>
+                                <th>Công việc</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>2211562</td>
+                                <td>Vương Quang Khải</td>
+                                <td>Team leader / PM</td>
+                            </tr>
+                            <tr>
+                                <td>2213316</td>
+                                <td>Lê Văn Thoại</td>
+                                <td>Frontend dev</td>
+                            </tr>
+                            <tr>
+                                <td>2113786</td>
+                                <td>Dương Trọng Khôi</td>
+                                <td>Frontend dev</td>
+                            </tr>
+                            <tr>
+                                <td>2212935</td>
+                                <td>Đoàn Ngọc Hoàng Sơn</td>
+                                <td>Backend dev</td>
+                            </tr>
+                            <tr>
+                                <td>2213772</td>
+                                <td>Lê Đức Anh Tuấn</td>
+                                <td>Backend dev</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+
+                <div className="quote">
+                    <p>Coded with love and depression</p>
+                </div>
+            </div>
+        </>
+    );
+};
 
 export default Homepage;
