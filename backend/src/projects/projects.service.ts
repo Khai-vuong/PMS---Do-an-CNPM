137a804be07aba942e5dd7bf7c21cc51553e7b48
Le Duc Anh Tuan <tuanleqn04@gmail.com>
2024-10-20 18:45:27 +0700

    update CRUD generator for projects

--- backend/src/projects/projects.service.ts
+++ backend/src/projects/projects.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from "@nestjs/common";
 import { PrismaService } from "prisma/prisma.service";
-import { CreateProjectDto } from "./dtos/create-projects.dto";
 import { User } from "@prisma/client";
+import { UpdateProjectDto } from "./dtos/update-projects.dto";
+import { CreateProjectDto } from "./dtos/create-projects.dto";
 
 @Injectable()
 export class ProjectsService {
@@ -19,5 +20,60 @@ export class ProjectsService {
                 },
             },
         });
+    }
+
+    async updateProject(pid: string, updateProjectDto: UpdateProjectDto) {
+        const existingProject = await this.prisma.project.findUnique({
+            where: { pid },
+        });
+
+        if (!existingProject) {
+            throw new Error(`Project with pid ${pid} not found`);
+        }
+
+        return await this.prisma.project.update({
+            where: { pid },
+            data: {
+                ...updateProjectDto,
+                manager_ids: {
+                    connect: updateProjectDto.manager_ids?.map(managerId => ({ uid: managerId })) || [],
+                },
+                members: {
+                    connect: updateProjectDto.members?.map(memberId => ({ uid: memberId })) || [],
+                },
+            },
+        });
+    }
+
+    async deleteProject(pid: string) {
+        const existingProject = await this.prisma.project.findUnique({
+            where: { pid },
+        });
+
+        if (!existingProject) {
+            throw new Error(`Project with pid ${pid} not exist`);
+            return;
+        }
+
+        return await this.prisma.project.delete({
+            where: { pid },
+        })
+    }
+
+    async listProjects() {
+        return await this.prisma.project.findMany();
+    }
+
+    async getProject(pid: string) {
+        const existingProject = await this.prisma.project.findUnique({
+            where: { pid },
+        });
+
+        if (!existingProject) {
+            throw new Error(`Project with pid ${pid} not exist`);
+            return;
+        }
+
+        return existingProject;
     }
 }
