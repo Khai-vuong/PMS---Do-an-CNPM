1c17c58374231895e97606b99d8ade6813410113
Khai-vuong <khai.vuongquang@hcmut.edu.vn>
2024-12-01 10:40:26 +0700

    url

--- frontend/src/pages/Login/Login.tsx
+++ frontend/src/pages/Login/Login.tsx
@@ -11,7 +11,7 @@ const Login = () => {
     e.preventDefault();
 
     try {
-      const response = await axios.post("http://localhost:5174/auth/login", {
+      const response = await axios.post("http://localhost:4000/auth/login", {
         username: username,
         password: password,
       });
